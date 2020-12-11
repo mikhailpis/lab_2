8928427add08dbd047d57c1a9e9c22376a6fc2b5
mikhailpis <mikhailpis2001@gmail.com>
2020-12-11 14:02:28 +0300

    user

---
+++ user.py
@@ -0,0 +1,6 @@
+username = input("What is your name?")
+age = input("How old are you?")
+Livein = input("Where are you live?")
+print("This is Vyacheslav")
+print("Is is 18")
+print("(S)He live in Stavropol")
