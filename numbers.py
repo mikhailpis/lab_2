03a0ceb95315ebb2ca2c46bd8a45d7f0c8bcea73
mikhailpis <mikhailpis2001@gmail.com>
2020-12-13 14:12:10 +0300

    Задание 3

---
+++ numbers.py
@@ -0,0 +1,7 @@
+print("Введите любые четыре числа:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+d = float(input("d = "))
+i = (a + b)/(c + d)
+print("Результат вычислений:", "%.2f" % i)
