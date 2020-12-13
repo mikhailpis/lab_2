6ac8fcc6602e31a3ccab58ad4341e5b466f2deeb
mikhailpis <mikhailpis2001@gmail.com>
2020-12-13 14:43:04 +0300

    Индивидуальное

---
+++ individual.py
@@ -0,0 +1,8 @@
+u1 = float(input("Введите u1 "))
+u2 = float(input("Введите u2 "))
+S = float(input("Введите S "))
+P = S+(0.5*(u1-u2))
+print('Растояние между ними через 30 минут = ')
+print(P)
+
+
